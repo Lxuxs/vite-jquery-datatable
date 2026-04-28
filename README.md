b314b17f5ec3dff7db000197b0ef0732a747b5b6
René Elizalde <reroes799@gmail.com>
2026-04-28 09:15:58 -0500

    archivos iniciales para la prueba

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vite-jquery-datatable
+
+## Tecnologías
+
+* Usar node 22
+* npm install datatables.net-dt
+*
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import './style.css';
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
+
+const API_URL = "https://raw.githubusercontent.com/PlataformasWeb-P-AA2026/api-demo/main/db.json"
+
+async function cargarDatos() {
+  try {
+    const respuesta = await fetch(API_URL);
+
+    if (!respuesta.ok) {
+      throw new Error('Error al consumir la API');
+    }
+
+    const datos = await respuesta.json();
+
+    new DataTable('#tabla-posts', {
+      data: datos,
+      columns: [
+        { data: 'AMIE' },
+        { data: 'Nombre-Educativa' },
+        { data: 'Canton' },
+        { data: 'Parroquia' }
+      ],
+      pageLength: 10,
+      language: {
+        search: 'Buscar:',
+        lengthMenu: 'Mostrar _MENU_ registros',
+        info: 'Mostrando _START_ a _END_ de _TOTAL_ registros',
+        paginate: {
+          previous: 'Anterior',
+          next: 'Siguiente'
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    document.querySelector('#app').innerHTML += `
+      <p class="error">No se pudieron cargar los datos.</p>
+    `;
+  }
+}
+
+cargarDatos();
